ebebee3fa8679e0a6f8e7b546644d5a9952659c7
Cvetikkk <solnecnij_svetik@mail.ru>
2022-11-26 22:57:40 +0300

    Модуль 13 init

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    """
+    Сумма за билеты по кол-ву посетителей
+    """
+    num = int(input("Введите количество билетов: "))
+    money = 0
+    count = 0
+
+    while count != num:
+        age = int(input(f"Введите возраст посетителя №{count+1}: "))
+        if age >= 25:
+            money += 1390
+            count += 1
+        elif age >= 18:
+            money += 990
+            count += 1
+        elif age >= 0:
+            count += 1
+
+    if count > 3:
+        money = int((money / 100) * 90)
+
+    print("Сумма к оплате", money)
+
+
+main()
